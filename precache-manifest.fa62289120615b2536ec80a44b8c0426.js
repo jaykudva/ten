303c1665364ddb9857cd85629c604445c40141f2
Jay Kudva <jaykudva@gmail.com>
2020-05-19 22:45:20 -0700

    Updates

---
+++ precache-manifest.fa62289120615b2536ec80a44b8c0426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee885c2d5db1e6121d1c06ed9180c110",
+    "url": "/ten/index.html"
+  },
+  {
+    "revision": "cbd6eae51e684edd0c7a",
+    "url": "/ten/static/css/main.099944a4.chunk.css"
+  },
+  {
+    "revision": "8c92dae359551edce9ab",
+    "url": "/ten/static/js/2.db4a6cb6.chunk.js"
+  },
+  {
+    "revision": "11cfc49acd1819f3e804b64601daa210",
+    "url": "/ten/static/js/2.db4a6cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd6eae51e684edd0c7a",
+    "url": "/ten/static/js/main.9e31b691.chunk.js"
+  },
+  {
+    "revision": "28f52ecd5f68f85c39e4",
+    "url": "/ten/static/js/runtime-main.d8b6d9af.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ten/precache-manifest.fa62289120615b2536ec80a44b8c0426.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ten/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
