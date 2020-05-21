e585198785cd840d5600ea81b28ed9a59a857e9f
Jay Kudva <jaykudva@gmail.com>
2020-05-20 22:24:19 -0700

    fixed and changed font, removed background gradient, implemented dark mode, fixed coloring on hyperlinks, added nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Darkmode from 'darkmode-js';
 import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
+
+var options = {
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.2s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#d8d3ca',  // default: '#fff'
+  buttonColorDark: '#272c35',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: String.fromCodePoint(0x1F313), // default: ''
+  autoMatchOsTheme: true // default: true
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
 
 class App extends Component {
   x = 50
@@ -17,6 +33,7 @@ class App extends Component {
       list: [],
     }
   }
+  
   getToken() { 
     var request = require('request'); // "Request" library
 
@@ -64,15 +81,43 @@ class App extends Component {
       })
     })
   }
+  componentDidMount() {
+    this.getToken()
+    
+  }
   render() {
     return (
       <div className= "center-v">
+        <nav>
+            <ul>
+                <li>
+                    <a href="https://jaykudva.me">home</a>
+                </li>
+                <li>
+                    <a href="/resume">resume</a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCuKdN2q-1Cv1kJRlAPT9tIg?sub_confirmation=1">youtube</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/jaykudva">linkedin</a>
+                </li>
+                <li>
+                    <a href="https://www.unsplash.com/@jaykudva">photography</a>
+                </li>
+                <li>
+                    <a href="/list.html">previous</a>
+                </li>
+                <li>
+                    <a href="/ten">top10</a>
+                </li>
+            </ul>
+        </nav>
           <div className="bounds">
             <div>
-              {this.getToken()}
               {
                 this.state.list.map((song, i) => {
-                  return <p><img src={song.track.album.images[0].url} style={{ height: this.x }}/> {i+1}: <a href={song.track.external_urls.spotify} target="_blank">{song.track.name}</a></p>
+                  return <p><img src={song.track.album.images[0].url} style={{ height: this.x }}/> {i+1}: <a href={song.track.external_urls.spotify} target="_blank" className="ooga">{song.track.name}</a></p>
                 })
               }
             </div>
@@ -85,7 +130,7 @@ class App extends Component {
             </button>
           </div>
       </div>
-    );
+      );
   }
 }
 
